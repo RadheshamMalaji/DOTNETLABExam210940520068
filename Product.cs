4637f634bc3438ae56d700d59fa5b1fd9feb875d
RadheshamMalaji <94758193+RadheshamMalaji@users.noreply.github.com>
2022-02-19 17:59:17 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabExam210940520068.Models
+{
+    public class Product
+    {
+        [Key]
+        [DisplayName("Product Id")]
+        public int ProductId { set; get; }
+
+        [DataType(DataType.Text)]
+        [DisplayName("Product Name")]
+        [Required(ErrorMessage ="Enter Valid Product Name")]
+        public string ProductName { set; get; }
+
+        [DisplayName("Rate")]
+        [Range(0.0,500.00,ErrorMessage ="Enter Rate Between 0 to 500")]
+        public decimal Rate { set; get; }
+
+        [DisplayName("Description")]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Enter Description")]
+        public string Description { set; get; }
+
+        [DisplayName("Category Name")]
+        [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Enter CategoryName")]
+        public string CategoryName { set; get; }
+    }
+}
---
+++ ProductsController.cs
@@ -0,0 +1,124 @@
+﻿using LabExam210940520068.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabExam210940520068.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+            SqlConnection sqlcn = new SqlConnection();
+            sqlcn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LabExam210940520068;Integrated Security=True;";
+            sqlcn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = sqlcn;
+            cmd.CommandText = "Select * from Products";
+            SqlDataReader data = cmd.ExecuteReader();
+            while(data.Read())
+            {
+                Product p = new Product();
+                p.ProductId = (int)data["ProductId"];
+                p.ProductName = data["ProductName"].ToString();
+                p.Description = data["Description"].ToString();
+                p.Rate = (decimal)data["Rate"];
+                p.CategoryName = data["CategoryName"].ToString();
+                ViewBag.id = data["ProductId"];
+                products.Add(p);
+            }
+            sqlcn.Close();
+            return View(products);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id,Product p)
+        {
+            try
+            {
+                List<Product> products = new List<Product>();
+                SqlConnection sqlcn = new SqlConnection();
+                sqlcn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LabExam210940520068;Integrated Security=True;";
+                sqlcn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = sqlcn;
+                cmd.CommandText = "update Products set ProductName=@ProductName,Description=@Description,Rate=@Rate,CategoryName=@CategoryName where ProductId=@ProductId";
+                cmd.Parameters.AddWithValue("@ProductName", p.ProductName);
+                cmd.Parameters.AddWithValue("@Description", p.Description);
+                cmd.Parameters.AddWithValue("@Rate", p.Rate);
+                cmd.Parameters.AddWithValue("@CategoryName", p.CategoryName);
+                cmd.Parameters.AddWithValue("@ProductId", id);
+                cmd.ExecuteNonQuery();
+                sqlcn.Close();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
